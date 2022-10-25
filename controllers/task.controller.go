5d72523e7b90fb767c143a438edc778f59b03e16
mo <dayoujianghong@gmail.com>
2022-10-25 21:26:55 +0900

    add models/task.models.go

--- controllers/task.controller.go
+++ controllers/task.controller.go
@@ -1,4 +1,44 @@
 package controllers
 
+import (
+	"net/http"
+
+	"example.com/crt-11/models"
+	"github.com/gin-gonic/gin"
+)
+
+type TaskService interface {
+	CreateTask(*models.Task) error
+}
+
 type TaskController struct {
+	TaskService TaskService
+}
+
+func New(taskservice TaskService) TaskController {
+	return TaskController{
+		TaskService: taskservice,
+	}
+}
+
+// var taskCollection *mongo.Collection = configs.GetCollection(configs.DB, "tasks")
+// var taskValidate = validator.New()
+
+func (ts *TaskController) CreateTask(ctx *gin.Context) {
+	var task models.Task
+	if err := ctx.ShouldBind(&task); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := ts.TaskService.CreateTask(&task)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": err.Error()})
+}
+
+func (ts *TaskController) RegisterTaskRoutes(rg *gin.RouterGroup) {
+	taskroute := rg.Group("/task")
+	taskroute.POST("/create", ts.CreateTask)
 }
--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"example.com/crt-11/bots"
 	"example.com/crt-11/configs"
+	"example.com/crt-11/controllers"
 	"example.com/crt-11/routes"
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	server *gin.Engine
+	tc     controllers.TaskController
 )
 
 func main() {
@@ -17,6 +23,9 @@ func main() {
 	routes.UserRoute(r)
 
 	routes.CityRoute(r)
+
+	basepath := server.Group("/v1")
+	tc.RegisterTaskRoutes(basepath)
 
 	r.Run()
 }
